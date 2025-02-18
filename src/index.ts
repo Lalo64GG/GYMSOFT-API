fb194cb6c3ec30c4807f30137fb977cadca1d93f
saul201287 <84940926+saul201287@users.noreply.github.com>
2025-02-18 12:28:01 -0600

    bases para levantar el server de la API y la conexion de la base de datos

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-// SAUL MAMPO
+import { app } from "./cmd/server"
+import { Signale } from "signale";
+import { database } from "./db/Database";
+const logger = new Signale({
+  secrets: ["([0-9]{4}-?)+"],
+});
+
+const port = process.env.PORT || 3000;
+
+try {
+  //database.connect();
+  console.log("Servidor iniciado correctamente");
+  app.listen(port, () => {
+    logger.success("Server listening on port:", port);
+  });
+} catch (error) {
+  console.error("No se pudo iniciar el servidor:", error);
+}
