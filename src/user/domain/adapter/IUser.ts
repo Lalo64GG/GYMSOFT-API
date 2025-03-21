ba1003ea226b45d8d49fe6d2fb24b8390c1c41fb
saul201287 <84940926+saul201287@users.noreply.github.com>
2025-03-20 21:20:16 -0600

    casi

--- src/user/domain/adapter/IUser.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "../entity/User";
-
-export interface IUser {
-    createUser(user: User): Promise<User>;
-    getAllUsers(): Promise<User[]>;
-    getUserById(id: number): Promise<User>;
-    updateUser(user: User): Promise<User>;
-    deleteUser(id: number): Promise<boolean>;
-}
